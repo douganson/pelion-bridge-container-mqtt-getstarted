732e40489980edbf6df88eb92b422c8bac354e01
Doug Anson <doug.anson@arm.com>
2018-09-28 02:12:23 -0500

    updates

--- start-bridge.sh
+++ start-bridge.sh
@@ -42,4 +42,4 @@ fi
 
 set -x
 
-docker run -d -p ${IP}:17362:17362 -p ${IP}:3883:1883 -p ${IP}:2880:1880 -p ${IP}:28520:28520 -p ${IP}:2222:22 -p ${IP}:2883:1883 -p ${IP}:8234:8234 -t mbed/pelion-bridge  /home/arm/start_instance.sh
+docker run -d -p ${IP}17362:17362 -p ${IP}3883:1883 -p ${IP}2880:1880 -p ${IP}28520:28520 -p ${IP}2222:22 -p ${IP}2883:1883 -p ${IP}8234:8234 -t mbed/pelion-bridge  /home/arm/start_instance.sh
